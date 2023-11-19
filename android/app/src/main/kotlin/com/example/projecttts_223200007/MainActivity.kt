42513eaf3b2c0c63fe47fcfd6c41e8ea96e3f4dc
fahiem152 <amfahiem010502@gmail.com>
2023-11-19 21:55:15 +0700

    project

---
+++ android/app/src/main/kotlin/com/example/projecttts_223200007/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fahiem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/common/config.dart
@@ -0,0 +1,4 @@
+class Config {
+  static const String apiKey = 'bce0718b69ea9ad628a297f2645e717f';
+  static const String baseUrl = 'https://api.rajaongkir.com/starter';
+}
